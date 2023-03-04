f48f4d52468d2f3e4a6fb7d55dcec43991bb367f
ayushh22 <ayusingh@pm.me>
2023-03-04 18:48:37 +0530

    code6.1

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -1,6 +1,8 @@
 package com.driver.services.impl;
 
 import com.driver.model.Admin;
+import com.driver.model.Country;
+import com.driver.model.CountryName;
 import com.driver.model.ServiceProvider;
 import com.driver.repository.AdminRepository;
 import com.driver.repository.CountryRepository;
@@ -43,6 +45,39 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception{
+        if (countryName.equalsIgnoreCase("ind") || countryName.equalsIgnoreCase("usa")|| countryName.equalsIgnoreCase("aus")|| countryName.equalsIgnoreCase("chi")|| countryName.equalsIgnoreCase("jpn")) {
+            ServiceProvider serviceProvider= serviceProviderRepository1.findById(serviceProviderId).get();
+            Country country= new Country();
 
+            if(countryName.equalsIgnoreCase("ind")) {
+                country.setCountryName(CountryName.IND);
+                country.setCode(CountryName.IND.toCode());
+            }
+
+            if(countryName.equalsIgnoreCase("usa")) {
+                country.setCountryName(CountryName.USA);
+                country.setCode(CountryName.USA.toCode());
+            }
+
+            if(countryName.equalsIgnoreCase("aus")) {
+                country.setCountryName(CountryName.AUS);
+                country.setCode(CountryName.AUS.toCode());
+            }
+
+            if(countryName.equalsIgnoreCase("chi")) {
+                country.setCountryName(CountryName.CHI);
+                country.setCode(CountryName.CHI.toCode());
+            }
+
+            if(countryName.equalsIgnoreCase("jpn")) {
+                country.setCountryName(CountryName.JPN);
+                country.setCode(CountryName.JPN.toCode());
+            }
+
+            country.setServiceProvider(serviceProvider);
+            serviceProvider.getCountryList().add(country);
+            serviceProviderRepository1.save(serviceProvider);
+            return serviceProvider;
+        } else throw new Exception("Country not Added!");
     }
 }
